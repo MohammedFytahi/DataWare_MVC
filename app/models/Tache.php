80b3cdf0d21ae4c9386d8bb1d131cf3cef21f751
MohammedFytahi <mohammedfytahi567@gmail.com>
2024-01-04 14:59:17 +0100

    to do

--- app/models/Tache.php
+++ app/models/Tache.php
@@ -1,6 +1,5 @@
-<?php
-// Tache.php
 
+<?php
 class Tache {
     private $db;
 
@@ -9,35 +8,37 @@ class Tache {
     }
 
     public function getTachesByUserId($userId) {
-        $this->db->query('SELECT t.*, p.nom_projet AS nom_projet FROM taches t 
+        $this->db->query('SELECT t.*, p.nom_projet AS nom_projet, t.Nome_Tache, t.statut FROM taches t 
         INNER JOIN projets p ON t.Project_ID = p.id_Project 
         WHERE t.User_Id = :user_id AND t.Project_ID = p.id_Project');
 
-// Liaison de la valeur du paramètre
-$this->db->bind(':user_id', $userId);
+        $this->db->bind(':user_id', $userId);
 
-$results = $this->db->resultSet();
+        $results = $this->db->resultSet();
 
-return $results;
+        return $results;
     }
 
-    public function addTache($data) {
-        $this->db->query('INSERT INTO taches (Nome_Tache, Date_debut, Date_fin, statut, Project_ID, User_Id) VALUES (:nome_tache, :date_debut, :date_fin, :statut, :project_id, :user_id)');
-        // Bind values
-        $this->db->bind(':nome_tache', $data['nome_tache']);
-        $this->db->bind(':date_debut', $data['date_debut']);
-        $this->db->bind(':date_fin', $data['date_fin']);
-        $this->db->bind(':statut', $data['statut']);
-        $this->db->bind(':project_id', $data['project_id']);
-        $this->db->bind(':user_id', $data['user_id']);
 
-        // Execute
-        return $this->db->execute();
+
+    public function addTaches($data)
+    {
+        $this->db->query('INSERT INTO taches (Nome_Tache, Date_debut, Date_fin, user_id, Project_ID) VALUES (:Nom_Tache, :Date_debut, :Date_fin, :user_id, :Project_ID)');
+        $this->db->bind(':Nom_Tache', $data['Nom_Tache']);
+        $this->db->bind(':Date_debut', $data['date_debut']);
+        $this->db->bind(':Date_fin', $data['date_fin']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':Project_ID', $data['Project_ID']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function updateTache($data) {
         $this->db->query('UPDATE taches SET Nome_Tache = :nome_tache, Date_debut = :date_debut, Date_fin = :date_fin, statut = :statut, Project_ID = :project_id WHERE id_tache = :tache_id');
-        // Bind values
         $this->db->bind(':tache_id', $data['tache_id']);
         $this->db->bind(':nome_tache', $data['nome_tache']);
         $this->db->bind(':date_debut', $data['date_debut']);
@@ -45,16 +46,45 @@ return $results;
         $this->db->bind(':statut', $data['statut']);
         $this->db->bind(':project_id', $data['project_id']);
 
-        // Execute
         return $this->db->execute();
     }
 
-    public function deleteTache($tacheId) {
+    public function deleteTache($id)
+    {
         $this->db->query('DELETE FROM taches WHERE id_tache = :tache_id');
-        // Bind values
-        $this->db->bind(':tache_id', $tacheId);
+        $this->db->bind(':tache_id', $id);
 
-        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function updateStatus($id, $newStatus)
+    {
+        $this->db->query('UPDATE taches SET statut = :new_status WHERE id_tache = :tache_id');
+        $this->db->bind(':new_status', $newStatus);
+        $this->db->bind(':tache_id', $id);
+
         return $this->db->execute();
     }
+
+    public function searchTaches($userId, $searchQuery)
+{
+    $this->db->query('SELECT t.*, p.nom_projet AS nom_projet, t.Nome_Tache, t.statut 
+                      FROM taches t 
+                      INNER JOIN projets p ON t.Project_ID = p.id_Project 
+                      WHERE t.User_Id = :user_id 
+                      AND t.Project_ID = p.id_Project 
+                      AND t.Nome_Tache LIKE :search_query');
+
+    $this->db->bind(':user_id', $userId);
+    $this->db->bind(':search_query', '%' . $searchQuery . '%');
+
+    $results = $this->db->resultSet();
+
+    return $results;
 }
+
+}
+?>
--- app/views/projets/index.php
+++ app/views/projets/index.php
@@ -1,12 +1,13 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php flash('projet_message'); ?>
+
 <div class="row mb-3">
     <div class="col-md-6">
         <h1>Projets</h1>
     </div>
-    <div class="col-md-6">
-        <a href="<?php echo URLROOT; ?>/projets/add" class="btn btn-primary pull-right">
-            <i class="fa fa-pencil"></i> Add Projets
+    <div class="col-md-6 text-right">
+        <a href="<?php echo URLROOT; ?>/projets/add" class="btn btn-primary">
+            <i class="fa fa-pencil"></i> Ajouter Projet
         </a>
     </div>
 </div>
@@ -15,32 +16,31 @@
 $user_id = $_SESSION['user_id'];
 
 foreach ($data['projets'] as $projet):
-    if ($projet->userId == $user_id):
-        ?>
-        <div class="card card-body mb-3">
-            <h4 class="card-title">
-                <?php echo $projet->nom_projet; ?>
-            </h4>
-            <div class="bg-light p-2 mb-3">
-                Created by
-                <?php echo $projet->name; ?> on
-                <?php echo $projet->projetCreated; ?>
-            </div>
-            <a href="<?php echo URLROOT; ?>/taches/index/<?php echo $projet->projetId; ?>" class="btn btn-primary">Tasks</a>
+    if ($projet->userId != $user_id) {
+        continue;
+    }
+    ?>
 
-            <?php if ($projet->userId == $_SESSION['user_id']): ?>
-                <hr>
-                <a href="<?php echo URLROOT; ?>/projets/edit/<?php echo $projet->projetId; ?>" class="btn btn-dark">Edit</a>
+    <div class="card card-body mb-3">
+        <h4 class="card-title"><?php echo $projet->nom_projet; ?></h4>
 
-                <form class="pull-right" action="<?php echo URLROOT; ?>/projets/delete/<?php echo $projet->projetId; ?>"
-                    method="post">
-                    <input type="submit" value="Delete" class="btn btn-danger">
-                </form>
-            <?php endif; ?>
+        <div class="bg-light p-2 mb-3">
+            Créé par <?php echo $projet->name; ?> le <?php echo $projet->projetCreated; ?>
         </div>
-        <?php
-    endif;
-endforeach;
-?>
+
+        <a href="<?php echo URLROOT; ?>/taches/index/<?php echo $projet->projetId; ?>" class="btn btn-primary">Tâches</a>
+
+        <?php if ($projet->userId == $_SESSION['user_id']): ?>
+            <div class="mt-3">
+                <a href="<?php echo URLROOT; ?>/projets/edit/<?php echo $projet->projetId; ?>" class="btn btn-dark">Modifier</a>
+
+                <form class="d-inline" action="<?php echo URLROOT; ?>/projets/delete/<?php echo $projet->projetId; ?>" method="post">
+                    <input type="submit" value="Supprimer" class="btn btn-danger">
+                </form>
+            </div>
+        <?php endif; ?>
+    </div>
+
+<?php endforeach; ?>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
